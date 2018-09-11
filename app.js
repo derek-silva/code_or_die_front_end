2a545dd71f7304b6ec2a2d2a5281c4da371d9404
github username <github email address>
2018-09-10 22:31:24 -0500

    added css styling and html nav bar and form

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const URL = `http://localhost:3000/users`;
 
 // Event Listeners
 document.addEventListener("DOMContentLoaded", getUsers);
+document.addEventListener("DOMContentLoaded", initMap);
+
+// Functions for pop-up forms
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
 // GET users from API
 function getUsers() {
   fetch(URL)
     .then(res => res.json())
     .then(console.log);
+}
+
+function initMap() {
+  var options = {
+    zoom: 12,
+    center: { lat: 29.7604, lng: -95.3698 }
+  };
+
+  var map = new google.maps.Map(document.getElementById("map"), options);
 }
